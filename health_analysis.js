380d1866ef2b5768ef5ad3ee7e9798df4c136512
H4Disrum <dmrakkesh@gmail.com>
2026-05-05 01:53:05 +0530

    Defined Variables

---
+++ health_analysis.js
@@ -0,0 +1,4 @@
+const addPatient = document.getElementById('addPatient');
+const report = document.getElementById('report');
+const btnSearch = document.getElementById('btnSearch');
+const patients = [];
