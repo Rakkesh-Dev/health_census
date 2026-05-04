8b4946a16abe6bc8475928788cc233d5797eec9f
H4Disrum <dmrakkesh@gmail.com>
2026-05-05 03:43:49 +0530

    form reset

--- health_analysis.js
+++ health_analysis.js
@@ -2,3 +2,10 @@ const addPatient = document.getElementById('addPatient');
 const report = document.getElementById('report');
 const btnSearch = document.getElementById('btnSearch');
 const patients = [];
+
+function resetForm () {
+    document.getElementById('name').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('condition').value = '';
+    document.querySelector('input[name="gender"] : checked').checked = false;
+}
